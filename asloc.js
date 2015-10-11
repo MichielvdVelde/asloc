964ee62ff8c61c0522b0436feda08636d2407f66
Michiel van der Velde <michiel@michielvdvelde.nl>
2015-10-11 19:45:20 +0200

    Continued working

--- asloc.js
+++ asloc.js
@@ -6,7 +6,7 @@
 
 var options = {
     'dir': __dirname,
-    'recursive': true,
+    'recursive': false,
     'filter': false,
     'ignoreComments': true
 };
@@ -18,25 +18,41 @@ var options = {
 // Search the specified directory with default options
 
 var program = require('commander');
+var fs = require('fs');
 var recursive = require('recursive-readdir');
 var pkg = require('package');
 
 /**
+ * List all files in the directory, possibly based on the filter
+**/
+var listFilesInDirectory = function(callback) {
+
+    var listFiles = function(cb) {
+        //
+    };
+    var listFilesRecursive = function(cb) {
+        //
+    };
+
+    var listFunc = (options.recursive) ? listFiles : listFilesRecursive;
+    listFunc(function(err, files) {
+        //
+    });
+
+};
+
+/**
  * Function that actually loads the files and counts the SLOCs
 */
-var countSLOC = function() {
+var countSLOC = function(callback) {
 
-    var files = recursive(option.dir, function(err, files) {
-
-        //
-
-    });
+    //
 
 };
 
 program
     .version(pkg.version)
-    .option('-r, --recurive', 'Disable recursive listing')
+    .option('-r, --recurive', 'Enable recursive directory walking')
     .parse(process.argv);
 
 if(program.args.length >= 1) options.dir = program.args[0];
